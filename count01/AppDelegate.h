16aeebe3b5dfa9aa7adeb80f4d3dc499405b2a48
KAREN <karen@KAREN-no-MacBook-Pro.local>
2015-04-12 11:10:38 +0900

    Initial Commit

---
+++ count01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  count01
+//
+//  Created by KAREN on 2015/04/12.
+//  Copyright (c) 2015年 KAREN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ count01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  count01
+//
+//  Created by KAREN on 2015/04/12.
+//  Copyright (c) 2015年 KAREN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
